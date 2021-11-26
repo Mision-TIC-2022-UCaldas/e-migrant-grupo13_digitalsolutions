994b47c3ae3251ccb23e192f820997d7df880c42
williamvargas7 <86165574+williamvargas7@users.noreply.github.com>
2021-11-26 18:55:14 -0500

    Alistamiento entorno de desarrollo

--- Hackaton.app/Hackaton.App.Dominio/Entidades/Migrante.cs
+++ Hackaton.app/Hackaton.App.Dominio/Entidades/Migrante.cs
@@ -1,19 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Hackaton.App.Dominio.Entidades
 {
     public class Migrante
     {
         public int Id { get; set; }
-        //[Required, StringLength(50)]
+        [Required, StringLength(50)]
         public string Nombres { get; set; }
-        //[Required, StringLength(50)]
+        [Required, StringLength(50)]
         public string Apellidos { get; set; }
-        //[Required, StringLength(30)]
+        [Required, StringLength(30)]
         public string TipoDocumento { get; set; }
-        //[Required, StringLength(11)]
+        [Required, StringLength(11)]
         public string NumeroDocumento { get; set; }
-        //[Required, StringLength(11)]
+        [Required, StringLength(11)]
         public string PaisOrigen { get; set; }
-        //[Required, StringLength(11)]
+        [Required, StringLength(11)]
         public string FechaNacimiento { get; set; }
         public string DireccionElectronica { get; set; }
         public string NumeroTelefono { get; set; }
