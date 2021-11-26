59a129accfd427d860a5dbcfbf2603042fb91442
williamvargas7 <86165574+williamvargas7@users.noreply.github.com>
2021-11-26 10:43:59 -0500

    Alistamiento entorno de desarrollo

---
+++ Hackaton.app/Hackaton.App.Persistencia/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Hackaton.App.Persistencia
+{
+    public class Class1
+    {
+    }
+}
---
+++ Hackaton.app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hackaton.app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
