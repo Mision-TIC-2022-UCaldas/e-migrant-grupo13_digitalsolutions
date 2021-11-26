84e6ab94a39b3db6865df2ec3782e7894affe62b
williamvargas7 <86165574+williamvargas7@users.noreply.github.com>
2021-11-26 16:04:39 -0500

    Actualizacion entorno de desarrollo

---
+++ Hackaton.app/Hackaton.App.Dominio/Entidades/Entidad.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Hackaton.App.Dominio.Entidades
+{
+    public class Entidad
+    {
+        public int Id { get; set; }
+        //[Required, StringLength(50)]
+        public string RazonSocial { get; set; }
+        //[Required, StringLength(50)]
+        public string Nit { get; set; }
+        //[Required, StringLength(30)]
+        public string Direccion { get; set; }
+        //[Required, StringLength(11)]
+        public string Ciudad { get; set; }
+        //[Required, StringLength(11)]
+        public string DireccionElectronica { get; set; }
+        public string PaginaWeb { get; set; }
+        //[Required, StringLength(30)]
+        public string Sector { get; set; }
+        //[Required, StringLength(30)]
+        public string ServiciosOfrece { get; set; }
+    }
+}
