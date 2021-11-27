c1294c086cb019cc10a909d4ff86356253999482
williamvargas7 <86165574+williamvargas7@users.noreply.github.com>
2021-11-26 19:37:01 -0500

    Alistamiento entorno de desarrollo

---
+++ Hackaton.app/Hackaton.App.Persistencia/Conexion.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Hackaton.App.Dominio.Entidades;
+
+namespace Hackaton.App.Persistencia
+{
+    public class Conexion : DbContext
+    {
+        public DbSet<Migrante> Migrantes {get; set;}
+        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder conn){
+           if(!conn.IsConfigured){
+                conn.UseSqlServer("Data source = (localdb)\\MSSQLLocalDB; Initial Catalog = DigitalSolutions");
+           }
+       }
+    }
+}
---
+++ Hackaton.app/Hackaton.App.Persistencia/Migrations/20211127001900_Entidades.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hackaton.App.Persistencia.Migrations
+{
+    public partial class Entidades : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Migrantes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombres = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Apellidos = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    TipoDocumento = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    NumeroDocumento = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    PaisOrigen = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    FechaNacimiento = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    DireccionElectronica = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumeroTelefono = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DireccionActual = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Ciudad = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SituacionLaboral = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Migrantes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Migrantes");
+        }
+    }
+}
